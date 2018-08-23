5a980aac8fcc6534a2dd163d5b50fc2799b1ed54
Kopciuszek <adrian.kopcinski@microsoft.wsei.edu.pl>
2018-08-23 20:57:55 +0200

    start VueForm project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# to_do_list
+# VueForm
 
 > A Vue.js project
 
---
+++ src/components/StartPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="start_page">
+    <header> </header>
+    <router-link to="/vue">Go to Vue</router-link>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'start_page'
+}
+</script>
+
+<style scoped>
+
+</style>
