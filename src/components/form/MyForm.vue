783e0baf7f2bc4f1da0d9f876649184f3e26a1a1
Kopciuszek <adrian.kopcinski@microsoft.wsei.edu.pl>
2018-08-30 21:28:36 +0200

    add FormGroup and MyForm components

---
+++ src/components/form/MyForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <FormGroup description="siemanko" label="labelka"/>
+</template>
+
+<script>
+  import FormGroup from './FormGroup'
+
+  export default {
+    name: 'form',
+    components: {FormGroup},
+
+  }
+</script>
+
+
+<style scoped>
+
+</style>
